ef4e21b07260afc29395ca5647bff76afdf6dc03
Carlos Daniel Jesus <enderdragon5482@gmail.com>
2026-09-23 01:39:50 -0400

    Modificando funcionamiento del lector de .xls para el procesamiento de la columna IMAGEN

--- modulos/ingesta_excel.py
+++ modulos/ingesta_excel.py
@@ -1,5 +1,5 @@
-from turtle import st
 
+import streamlit as st
 import pandas as pd
 
 def cargar_inventario_excel(ruta_archivo):
@@ -32,6 +32,10 @@ def cargar_inventario_excel(ruta_archivo):
                 if 'IMAGEN' not in df_final.columns:
                     df_final['IMAGEN'] = None
 
+                df_final['PRECIO_USD'] = pd.to_numeric(df_final['PRECIO_USD'], errors='coerce')
+                df_final['STOCK'] = pd.to_numeric(df_final['STOCK'], errors='coerce').fillna(0)
+                
+                return df_final
                 
                 # Guardamos o concatenamos al catálogo general en st.session_state
                 # (aquí va tu lógica actual para guardar en st.session_state['catalogo'] o la BD)
@@ -39,16 +43,19 @@ def cargar_inventario_excel(ruta_archivo):
                 
             else:
                 st.error("❌ El archivo Excel no contiene todas las columnas requeridas (SKU, MATERIAL, PRECIO_USD, STOCK).")
-                
+                return None
+            
         except Exception as e:
             st.error(f"Error al procesar el archivo Excel: {e}")
-            
+            return None
         except FileNotFoundError:
-            print(f"[Error] No se encontró el archivo en la ruta: {ruta_archivo}")
+            st.error(f"[Error] No se encontró el archivo en la ruta: {ruta_archivo}")
             return None
         except ValueError as error_valor:
-            print(f"[Error de Formato] El Excel no tiene la estructura correcta. Detalles: {error_valor}")
+            st.error(f"[Error de Formato] El Excel no tiene la estructura correcta. Detalles: {error_valor}")
             return None
+        
+    return None
     # """
     # Lee un archivo Excel y verifica que tenga la estructura correcta.
     # Retorna un DataFrame de Pandas o None si hay un error.
